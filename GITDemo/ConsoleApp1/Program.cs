e35ab1fa9ef4bf1f5ae51240f20c77b4786f5e32
sayali1396 <118646134+sayali1396@users.noreply.github.com>
2023-06-15 09:00:42 +0530

    added method with calculaator task

--- GITDemo/ConsoleApp1/Program.cs
+++ GITDemo/ConsoleApp1/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
 Console.WriteLine();
 Console.WriteLine("Hello sayali");
+Console.WriteLine("Calculator Task:");
+Console.WriteLine("Enter First No:");
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second No. ");
+int b= int.Parse(Console.ReadLine());   
+Calculator c1= new Calculator();
+c1.PrintA(a,b);
 Console.ReadLine(); 
