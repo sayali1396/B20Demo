0b984715839a7ac4549f19dc25f437ff83e6115a
sayali1396 <118646134+sayali1396@users.noreply.github.com>
2023-06-15 09:51:54 +0530

    mul operation added with program.cs file

--- GITDemo/ConsoleApp1/Program.cs
+++ GITDemo/ConsoleApp1/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
 Console.WriteLine("Hello, World!");
 Console.ReadLine();
 Console.WriteLine();
 Console.WriteLine("Hello sayali");
+Console.WriteLine(  "Multiplication opeartion staeted");
+Console.WriteLine("Enter 1st no:");
+    int a= int.Parse(Console.ReadLine());
+Console.WriteLine("Enter 2 nd no.");
+int b= int.Parse(Console.ReadLine());
+mul m = new mul();
+m.multi(4, 5);
 Console.ReadLine(); 
---
+++ GITDemo/ConsoleApp1/mul.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+public class mul
+    {
+        public void multi(int a, int b)
+        {
+            Console.WriteLine($"mul result is:{a*b}");
+        }
+    }
+}
